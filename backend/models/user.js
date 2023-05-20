81796e7e3586223575e87ad9a72a56b469827eaf
leslieportiz <lorraineportiz@gmail.com>
2023-05-19 22:57:01 -0700

    Completed IAM5 tasks.

--- backend/models/user.js
+++ backend/models/user.js
@@ -10,22 +10,30 @@ module.exports = (sequelize, DataTypes) => {
     }
 
   };
+
   User.init({
     userId: {
       type: DataTypes.SMALLINT,
       primaryKey: true,
       autoIncrement: true
-
     },
     firstName: DataTypes.STRING,
     lastName: DataTypes.STRING,
     email: DataTypes.STRING,
+    role: {
+      type: DataTypes.ENUM,
+      values: [
+        'reviewer',
+        'admin',
+      ],
+    },
     passwordDigest: DataTypes.STRING
-  }, {
-    sequelize,
-    underscored: true,
-    modelName: 'User',
-  });
+  },
+    {
+      sequelize,
+      underscored: true,
+      modelName: 'User',
+    });
   return User;
 };
-  
+
